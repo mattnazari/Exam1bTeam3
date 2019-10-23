8e297a3b39b07d5fbe24ad685bbf16e2f533727c
matthewnazari <matthew.nazari8@gmail.com>
2019-10-23 11:29:12 -0700

    added all comps

--- comps/Main.js
+++ comps/Main.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {View} from 'react-native';
 import Toast from './Toast';
+import Right from './Right';
+import Settings from './Settings';
+import Left from './Left';
 
 const Main = () => {
 
     return (
-      <View style={{flex:1}}>
+      <View style={{flex:1, flexDirection:'row'}}>
+        <Left/>
+        <Right/>
         <Toast />
+        {/* <Settings/> stops the other comps from working
+        because the image is in the comp and the comp itself takes
+        100% of the screen */}
       </View>
     )
   };
