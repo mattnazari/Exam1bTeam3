7ca9270bedfb0e0cc5bd2753e2a43b3bd1d34d8f
matthewnazari <matthew.nazari8@gmail.com>
2019-10-22 19:38:14 -0700

    Initial Setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import Main from './comps/Main';
+
+const App = () => {
+  return (
+    <View style={{flex:1}}>
+      <Main />
+    </View>
+  )
+}
+
+export default App;
---
+++ comps/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+const Main = () => {
+
+    return (
+      <View style={{flex:1}}>
+      </View>
+    )
+  };
+
+  export default Main;
